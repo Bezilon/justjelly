457b209d7d0472314b654e45c48ed38a1f9e8233
Balázs Kovács <bezilon@proton.me>
2025-04-24 15:28:32 +0300

    Removing orm from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-import { db } from '@/db/index'
-import { users } from '@/db/schema';
-import { eq } from 'drizzle-orm';
-
-export default async function Home() {
-  const adminUser = await (await db.select().from(users).where(eq(users.username, 'admin'))).pop()
-
+export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>Just Jelly</h1>
-
-        <h2>{adminUser?.username}</h2>
 
         <div>
           <input type="text"/>
