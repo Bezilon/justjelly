9405760205a744451a0c3e2a565fa3c523a4dff2
Balázs Kovács <bezilon@gmail.com>
2025-04-25 15:42:54 +0300

    Unused vars fix

--- src/app/components/RegisterForm.tsx
+++ src/app/components/RegisterForm.tsx
@@ -1,14 +1,10 @@
 'use client'
 
-import { useState } from "react";
-
 type NewUserFormProps = {
   firstUser: boolean;
 }
 
 const NewUserForm = ({firstUser}:NewUserFormProps) => {
-  const [ user, setUser ] = useState({ username: '', password: '', confirmPassword: '' });
-
   return <div>
     <h1 className="text-4xl font-bold">Welcome to JustJelly!</h1>
     <p className="text-lg mb-4">{ firstUser ? "Please create an admin user to get started:" : "Please enter your username and password to register" }</p>
